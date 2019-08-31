982336ff23d267f0cd2017190d958e2ce06793df
Nikhil Singh <nikhilsingh498@gmail.com>
2019-09-01 03:11:26 +0530

    Add db models

---
+++ Models/model.js
@@ -0,0 +1,30 @@
+
+const mongoose = require('mongoose');
+
+const user = new mongoose.Schema({
+  NAME: {
+    type: String,
+    required: [true, 'Name is required'],
+  },
+  EMAIL: {
+    type: String,
+    required: [true, 'Email is required'],
+    lowercase: true,
+    unique: true,
+  },
+  PASSWORD: {
+    type: String,
+    required: [true, 'Password is required'],
+	},
+	// Future prospect - profile pic upload
+  IMAGE_URL: {
+		type: String,
+		default: 'To be provided'
+  },
+  IMAGE_ID: {
+		type: String,
+		default: 'To be provided'
+  },
+});
+
+module.exports.user = mongoose.model('user', user);
