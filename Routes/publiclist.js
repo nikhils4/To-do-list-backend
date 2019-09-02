8a8bc66c0efd9490daa1664e8d92e801738c45f3
Nikhil Singh <nikhilsingh498@gmail.com>
2019-09-03 02:11:37 +0530

    Add operations for public user list

--- Routes/publiclist.js
+++ Routes/publiclist.js
@@ -76,4 +76,81 @@ router.post('/public/dellist', (request, response) => {
     });
 });
 
+// Add user from list of user with whom the list has been shared
+router.post('/public/adduser', (request, response) => {
+  User.findOneAndUpdate({
+    EMAIL: request.decode.email,
+  },
+  { $push: { PUBLIC_LIST_USER: request.body.list} },
+  { new: true })
+    .then((result) => {
+      if (result) {
+        response.status(200).json({
+          message: 'Public user list was successfully updated - added',
+        });
+      } else {
+        response.status(400).json({
+          message: 'Some error while fetching details',
+        });
+      }
+    })
+    .catch(() => {
+      response.status(500).json({
+        message: 'Internal Server Error',
+      });
+    });
+});
+
+
+// Delete user from list of user with whom the list has been shared
+router.post('/public/deluser', (request, response) => {
+  User.update({
+    EMAIL: request.decode.email,
+  },
+  { $pullAll: { PUBLIC_LIST_USER: [request.body.remove] } 
+})
+    .then((result) => {
+      if (result) {
+        response.status(200).json({
+          message: 'Public user list was successfully updated - deleted',
+        });
+      } else {
+        response.status(400).json({
+          message: 'Some error while fetching details',
+        });
+      }
+    })
+    .catch((err) => {
+			console.log(err)
+      response.status(500).json({
+        message: 'Internal Server Error',
+      });
+    });
+});
+
+
+// Get list of user who all share public list
+router.post('/public/getuser', (request, response) => {
+  User.findOne({
+    EMAIL: request.decode.email,
+  })
+    .then((result) => {
+      if (result) {
+        response.status(200).json({
+          list: result.PUBLIC_LIST_USER,
+        });
+      } else {
+        response.status(400).json({
+          message: 'Some error while fetching details',
+        });
+      }
+    })
+    .catch((err) => {
+			console.log(err)
+      response.status(500).json({
+        message: 'Internal Server Error',
+      });
+    });
+})
+
 module.exports = router;
