5f1f628ef5372e5078cdfcff7f2d07413fbab4f0
Nikhil Singh <nikhilsingh498@gmail.com>
2019-09-13 00:50:57 +0530

    Add route to get data

--- Routes/privatelist.js
+++ Routes/privatelist.js
@@ -2,6 +2,29 @@ const router = require('express').Router();
 const User = require('../Models/model.js').user;
 const middleware = require('../Helpers/middleware').session;
 
+// Get user data
+router.get('/getname',  middleware,  (request, response) => {
+  User.findOne({
+    EMAIL: request.decode.email,
+  })
+    .then((result) => {
+      if (result) {
+        response.status(200).json({
+          name: result.NAME,
+        });
+      } else {
+        response.status(200).json({
+          message: 'Some error while fetching details',
+        });
+      }
+    })
+    .catch((err) => {
+      response.status(500).json({
+        message: 'Internal Server Error',
+      });
+    });
+});
+
 // Add to private list
 router.post('/private/addlist',  middleware,  (request, response) => {
   User.findOneAndUpdate({
