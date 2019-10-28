67f359b1f0439fb8843f1a8c1a7879fb907b2fab
Nikhil Singh <nikhilsingh498@gmail.com>
2019-10-29 03:47:44 +0530

    Add user bug fix

--- Routes/publiclist.js
+++ Routes/publiclist.js
@@ -20,7 +20,7 @@ router.post('/public/getlist', middleware, (request, response) => {
       }
     })
     .catch((err) => {
-			console.log(err)
+      console.log(err)
       response.status(500).json({
         message: 'Internal Server Error',
       });
@@ -32,8 +32,8 @@ router.post('/public/addlist', middleware, (request, response) => {
   User.findOneAndUpdate({
     EMAIL: request.decode.email,
   },
-  { $push: { PUBLIC_LIST: request.body.list} },
-  { new: true })
+    { $push: { PUBLIC_LIST: request.body.list } },
+    { new: true })
     .then((result) => {
       if (result) {
         response.status(200).json({
@@ -57,8 +57,9 @@ router.post('/public/dellist', middleware, (request, response) => {
   User.update({
     EMAIL: request.decode.email,
   },
-  { $pullAll: { PUBLIC_LIST: [request.body.remove] } 
-})
+    {
+      $pullAll: { PUBLIC_LIST: [request.body.remove] }
+    })
     .then((result) => {
       if (result) {
         response.status(200).json({
@@ -71,7 +72,7 @@ router.post('/public/dellist', middleware, (request, response) => {
       }
     })
     .catch((err) => {
-			console.log(err)
+      console.log(err)
       response.status(500).json({
         message: 'Internal Server Error',
       });
@@ -80,44 +81,62 @@ router.post('/public/dellist', middleware, (request, response) => {
 
 // Add user from list of user with whom the list has been shared
 router.post('/public/adduser', middleware, (request, response) => {
-  User.findOneAndUpdate({
-    EMAIL: request.decode.email,
-  },
-  { $push: { PUBLIC_LIST_USER: request.body.list} },
-  { new: true })
+
+  User.findOne({
+    EMAIL: request.body.list,
+  })
     .then((result) => {
       if (result) {
         User.findOneAndUpdate({
-          EMAIL: request.body.list,
+          EMAIL: request.decode.email,
         },
-        { $push: { OTHER_USER_LISTS: request.decode.email} },
-        { new: true })
-        .then((res) => {
-          let email = `<p>Hey ${res.NAME},</p><p>Thanks for using Keep Notes.</p><p>${result.NAME} (${result.EMAIL}) has invited you to view and request for change on their public list :-</p><p>Login to view what they have to share with you.</p><p>For any assistance reach us out at <a href="mailto:snapnab.dev@gmail.com" style="text-decoration: none">support</a>.<p>Thanks<br>Team Keep Notes</p>`
-          if (mail(request.body.list, 'Public list invite', email)) {
-            response.status(200).json({
-              message: 'User was successfully added',
-            });
-          } else {
+          { $push: { PUBLIC_LIST_USER: request.body.list } },
+          { new: true })
+          .then((result) => {
+            if (result) {
+              User.findOneAndUpdate({
+                EMAIL: request.body.list,
+              },
+                { $push: { OTHER_USER_LISTS: request.decode.email } },
+                { new: true })
+                .then((res) => {
+                  let email = `<p>Hey ${res.NAME},</p><p>Thanks for using Keep Notes.</p><p>${result.NAME} (${result.EMAIL}) has invited you to view and request for change on their public list :-</p><p>Login to view what they have to share with you.</p><p>For any assistance reach us out at <a href="mailto:snapnab.dev@gmail.com" style="text-decoration: none">support</a>.<p>Thanks<br>Team Keep Notes</p>`
+                  if (mail(request.body.list, 'Public list invite', email)) {
+                    response.status(200).json({
+                      message: 'User was successfully added',
+                    });
+                  } else {
+                    response.status(500).json({
+                      message: 'There was some error while adding user',
+                    });
+                  }
+                })
+                .catch((err) => {
+                  response.status(200).json({
+                    message: 'Invalid input - user does not exist',
+                  });
+                })
+
+
+            } else {
+              response.status(200).json({
+                message: 'Invalid input - user does not exist',
+              });
+            }
+          })
+          .catch(() => {
             response.status(500).json({
-              message: 'There was some error while adding user',
+              message: 'Internal Server Error',
             });
-          }
-        })
-        .catch((err) => {
-          response.status(200).json({
-            message: 'Invalid input - user does not exist',
           });
-        })
-
-
       } else {
         response.status(200).json({
           message: 'Invalid input - user does not exist',
         });
       }
     })
-    .catch(() => {
+    .catch((err) => {
+      console.log(err)
       response.status(500).json({
         message: 'Internal Server Error',
       });
@@ -137,25 +156,26 @@ router.post('/public/deluser', middleware, (request, response) => {
         User.update({
           EMAIL: request.decode.email,
         },
-        { $pullAll: { PUBLIC_LIST_USER: [request.body.remove] } 
-      })
+          {
+            $pullAll: { PUBLIC_LIST_USER: [request.body.remove] }
+          })
           .then((result) => {
             if (result) {
               User.findOneAndUpdate({
                 EMAIL: request.body.list,
               },
-              { $pullAll: { OTHER_USER_LISTS: [request.decode.email]} },
-              { new: true })
-              .then((res) => {
-                response.status(200).json({
-                  message: 'User was successfully deleted',
-                });
-              })
-              .catch((err) => {
-                response.status(200).json({
-                  message: 'Invalid input - user does not exist',
-                });
-              })
+                { $pullAll: { OTHER_USER_LISTS: [request.decode.email] } },
+                { new: true })
+                .then((res) => {
+                  response.status(200).json({
+                    message: 'User was successfully deleted',
+                  });
+                })
+                .catch((err) => {
+                  response.status(200).json({
+                    message: 'Invalid input - user does not exist',
+                  });
+                })
             } else {
               response.status(200).json({
                 message: 'Invalid input - user does not exist',
@@ -175,7 +195,7 @@ router.post('/public/deluser', middleware, (request, response) => {
       }
     })
     .catch((err) => {
-			console.log(err)
+      console.log(err)
       response.status(500).json({
         message: 'Internal Server Error',
       });
@@ -201,7 +221,7 @@ router.get('/public/getuser', middleware, (request, response) => {
       }
     })
     .catch((err) => {
-			console.log(err)
+      console.log(err)
       response.status(500).json({
         message: 'Internal Server Error',
       });
@@ -225,7 +245,7 @@ router.post('/public/getlistothers', middleware, (request, response) => {
       }
     })
     .catch((err) => {
-			console.log(err)
+      console.log(err)
       response.status(500).json({
         message: 'Internal Server Error',
       });
